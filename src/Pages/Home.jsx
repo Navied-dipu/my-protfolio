9c88d62331b42fbd6a139a128e6f68515051bdfd
Navied-dipu <navieddipu08@gmail.com>
2025-11-18 00:46:37 +0600

    fix

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import Hero from "./Hero";
 import SkillCards from "./Skill";
-
-
 import ContactForm from "./ContactForm";
 import FeaturedProjects from "./featuredProjects";
+
 
 export default function Home() {
   return (
     <div id="Home">
       <Hero></Hero>
       <SkillCards></SkillCards>
-     <FeaturedProjects></FeaturedProjects>
+    <FeaturedProjects></FeaturedProjects>
       <ContactForm></ContactForm>
     </div>
   );
